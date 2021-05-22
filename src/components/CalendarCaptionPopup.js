63e1248d7ad9ea655c089fa8e1ebf9df1369a033
Victor Zhabolenko <kenzbrug1337@yandex.ru>
2021-05-22 10:19:38 +0300

    fix: цвет иконки, календарь на главной при авторизации

--- src/components/CalendarCaptionPopup.js
+++ src/components/CalendarCaptionPopup.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import CalendarParentPopup from './CalendarParentPopup';
+import React from "react";
+import CalendarParentPopup from "./CalendarParentPopup";
 
 function CalendarCaptionPopup() {
   return (
@@ -10,9 +10,7 @@ function CalendarCaptionPopup() {
           <p className="calendar__weekday">Декабрь / понедельник</p>
         </div>
         <div className="calendar__about">
-          <h2 className="section-title calendar__title calendar__title_type_popup">
-            Занятие с выпускниками: как составить резюме
-          </h2>
+          <h2 className="section-title calendar__title calendar__title_type_popup">Занятие с выпускниками: как составить резюме</h2>
           <p className="calendar__date">20</p>
         </div>
       </div>
@@ -22,9 +20,7 @@ function CalendarCaptionPopup() {
             <p className="calendar__time">12:00–14:00</p>
           </li>
           <li className="calendar__info-item">
-            <p className="calendar__place">
-              Садовническая наб., д. 77 стр. 1 (офис компании Ernst&Young)
-            </p>
+            <p className="calendar__place">Садовническая наб., д. 77 стр. 1 (офис компании Ernst&Young)</p>
           </li>
           <li className="calendar__info-item">
             <p className="calendar__contact">Александра, +7 926 356-78-90</p>
@@ -32,20 +28,13 @@ function CalendarCaptionPopup() {
         </ul>
         <div className="calendar__description">
           <p className="paragraph calendar__desc-paragraph">
-            Наконец-то наступила весна и мы пережили эту долгую зиму! И
-            возможно, что внутренних сил и ресурса сейчас не так много, а до
-            окончания учебного года ещё целых несколько месяцев. Поэтому
-            приглашаем вас на встречу нашего ресурсного клуба &quot;Наставник PRO&quot;,
-            которую мы хотим посвятить теме поиска моральных сил, смыслов и
-            внутреннего ресурса для общения и взаимодействия с нашими
-            подопечными.
+            Наконец-то наступила весна и мы пережили эту долгую зиму! И возможно, что внутренних сил и ресурса сейчас не так много, а до окончания
+            учебного года ещё целых несколько месяцев. Поэтому приглашаем вас на встречу нашего ресурсного клуба &quot;Наставник PRO&quot;, которую мы
+            хотим посвятить теме поиска моральных сил, смыслов и внутреннего ресурса для общения и взаимодействия с нашими подопечными.
           </p>
         </div>
         <div className="calendar__submit">
-          <button
-            className="button button_theme_light button_action_confirm"
-            type="button"
-          >
+          <button className="button button_theme_light button_action_confirm" type="button">
             Записаться
           </button>
           <p className="calendar__place-left">Осталось 5 мест</p>
@@ -53,6 +42,6 @@ function CalendarCaptionPopup() {
       </div>
     </CalendarParentPopup>
   );
-};
+}
 
 export default CalendarCaptionPopup;
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,13 +1,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const ProtectedRoute = ({component: Component, loggedIn}) => {
-  return (
-    <Route>
-      {()=> loggedIn ? <Component /> : <Redirect to="./sign-in" />}
-    </Route>
-)};
+const ProtectedRoute = ({ component: Component, loggedIn }) => {
+  return <Route>{() => (loggedIn ? <Component /> : <Redirect to="./sign-in" />)}</Route>;
+};
 
 export default ProtectedRoute;
