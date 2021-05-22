1274017d02f845a91429b3eae2dab24c982e3859
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-05-22 03:04:16 +0300

    feat: добавить деструктур. data в вызов testButton

--- src/components/App.js
+++ src/components/App.js
@@ -20,8 +20,8 @@ function App() {
   }
 
   function testButton() {
-    getListCities().then((res) => {
-      res.data.map((city) =>  console.log(city.name))
+    getListCities().then(({data}) => {
+      data.map((city) =>  console.log(city.name))
     })
     .catch((err) => {console.log(`Ошибка: ${err}`)})
   }
