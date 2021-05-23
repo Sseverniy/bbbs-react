6afcab16de34cd8a6f75d9803f8ff9d7cd0469f4
Victor Zhabolenko <kenzbrug1337@yandex.ru>
2021-05-23 18:36:29 +0300

    feat: подключить main к api, исправление ошибок prop-types

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,131 +1,132 @@
-import React from "react";
-import PropTypes from "prop-types";
-import WhereToGo from "./WhereToGo";
-import Films from "./Films";
-import Meetup from "./Meetup";
-import graduatesBBBS from "../sourse/sass/images/video/video-main.jpg";
-import calendarLogo from "../sourse/sass/images/svg/calendar_logo.svg";
-import historyMariaAndAlina from "../sourse/sass/images/stories/Maria-and-Alina.jpg";
+import React from 'react';
+import PropTypes from 'prop-types';
+import WhereToGo from './WhereToGo';
+import Movie from './Movie';
+import Meetup from './Meetup';
+import Question from './Question';
+// import graduatesBBBS from '../sourse/sass/images/video/video-main.jpg';
+import calendarLogo from '../sourse/sass/images/svg/calendar_logo.svg';
+// import historyMariaAndAlina from '../sourse/sass/images/stories/Maria-and-Alina.jpg';
 
-function Main({ loggedIn }) {
+function Main({ loggedIn, history, place, video, movies, questions, articles }) {
+  console.log(articles);
+  const secondsToHM = (time) => {
+    const h = Math.floor(time / 60);
+    const m = Math.floor(time % 60);
+
+    const hDisplay = h > 0 ? `${h}ч ` : '';
+    const mDisplay = m > 0 ? `${m}м ` : '';
+
+    return hDisplay + mDisplay;
+  };
+
   return (
     <>
-      <section className="lead page__section">
-        <article className="card-container card-container_type_identical">
+      <section className='lead page__section'>
+        <article className='card-container card-container_type_identical'>
           {loggedIn === true ? (
             <Meetup />
           ) : (
-            <article className="card card_color_green stub">
-              <div className="stub__upper-element">
-                <a href="./index.html">
-                  <img src={calendarLogo} alt="Логотип Старшие Братья Старшие Сестры России" className="stub__logo" />
+            <article className='card card_color_green stub'>
+              <div className='stub__upper-element'>
+                <a href='./index.html'>
+                  <img src={calendarLogo} alt='Логотип Старшие Братья Старшие Сестры России' className='stub__logo' />
                 </a>
               </div>
-              <div className="stub__content">
-                <h2 className="section-title stub__text">
+              <div className='stub__content'>
+                <h2 className='section-title stub__text'>
                   Наставники.про – цифоровая информационная платформа огрганизации «Старшие Братья Старшие Сестры».
                   Созданная для поддержки наставников программы.
                 </h2>
               </div>
             </article>
           )}
-          <article className="card card_content_media">
-            <img src={historyMariaAndAlina} alt="История Марины и Алины" className="card__media-img" />
-            <a href="./stories.html" className="card__media-link section-title">
-              История Марины и Алины
+          <article className='card card_content_media'>
+            <img src={history.imageUrl} alt='История Марины и Алины' className='card__media-img' />
+            <a href='./stories.html' className='card__media-link section-title'>
+              {history.title}
             </a>
           </article>
         </article>
       </section>
-      <WhereToGo />
-      <section className="main-section page__section">
-        <article className="card card_color_blue card_content_quote">
-          <a href="./articles.html" className="card__link-wrap">
-            <h3 className="chapter-title card__quote">
+      <WhereToGo place={place} />
+      <section className='main-section page__section'>
+        <article className='card card_color_blue card_content_quote'>
+          <a href='./articles.html' className='card__link-wrap'>
+            <h3 className='chapter-title card__quote'>
               Развитие детей-сирот отличается от развития детей, живущих в семьях. Все этапы развития у детей-сирот
               проходят с искажениями и имеют ряд негативных особенностей.
             </h3>
           </a>
-          <a href="./articles.html" className="link card__link">
+          <a href='./articles.html' className='link card__link'>
             читать статью
           </a>
         </article>
       </section>
-      <Films />
-      <section className="main-section page__section">
-        <article className="card-container card-container_type_main-video">
-          <div className="card card_color_yellow card_content_video-preview">
-            <div className="card__title-wrap">
-              <a href="./video.html" className="card__link-wrap">
-                <h2 className="section-title card__title">Эфир с выпускником нашей программы</h2>
+      <section className='main-section page__section cards-grid cards-grid_content_small-cards'>
+        {movies.map((movie) => (
+          <Movie
+            key={movie.id}
+            imageUrl={movie.imageUrl}
+            info={movie.info}
+            link={movie.link}
+            title={movie.title}
+            tags={movie.tags}
+          />
+        ))}
+      </section>
+      <section className='main-section page__section'>
+        <article className='card-container card-container_type_main-video'>
+          <div className='card card_color_yellow card_content_video-preview'>
+            <div className='card__title-wrap'>
+              <a href={video.link} className='card__link-wrap' rel='noreferrer' target='_blank'>
+                <h2 className='section-title card__title'>{video.title}</h2>
               </a>
-              <p className="caption card__title-caption">Иван Рустаев, выпускник программы</p>
+              <p className='caption card__title-caption'>{video.info}</p>
             </div>
-            <a href="123" className="link card__link link_action_open-video">
+            <a href={video.link} className='link card__link link_action_open-video' rel='noreferrer' target='_blank'>
               смотреть видео
             </a>
           </div>
-          <div className="card card_color_yellow card_content_video video">
-            <a href="./video.html" className="card__link-wrap">
-              <img src={graduatesBBBS} alt="Превью видео" className="video__img video__img_position_main-video" />
-              <p className="video__duration video__duration_position_main-video paragraph">59:44</p>
+          <div className='card card_color_yellow card_content_video video'>
+            <a href={video.link} className='card__link-wrap' rel='noreferrer' target='_blank'>
+              <img src={video.imageUrl} alt='Превью видео' className='video__img video__img_position_main-video' />
+              <p className='video__duration video__duration_position_main-video paragraph'>
+                {secondsToHM(video.duration)}
+              </p>
             </a>
           </div>
         </article>
       </section>
       <></>
-      <section className="main-section page__section">
-        <article className="card-container card-container_type_iframe">
-          <div className="card card_color_blue card_content_media">
+      <section className='main-section page__section'>
+        <article className='card-container card-container_type_iframe'>
+          <div className='card card_color_blue card_content_media'>
             <iframe
-              className="card__iframe"
-              title="facebook"
-              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=630&height=630&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-              scrolling="no"
-              // allowFullScreen="true"
-              allow="clipboard-write; encrypted-media; picture-in-picture; web-share"
+              className='card__iframe'
+              title='facebook'
+              src='https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=630&height=630&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
+              scrolling='no'
+              allow='clipboard-write; encrypted-media; picture-in-picture; web-share'
             />
           </div>
-          <div className="main-questions">
-            <article className="question main-questions__item">
-              <a href="./questions.html" className="main-questions__link">
-                <h2 className="section-title question__title main-questions__title">
-                  Я боюсь, что ребёнок ко мне слишком сильно привяжется. Что делать?
-                </h2>
-              </a>
-              <p className="rubric question__rubric main-questions__rubric">рубрика</p>
-            </article>
-
-            <article className="question main-questions__item">
-              <a href="./questions.html" className="main-questions__link">
-                <h2 className="section-title question__title main-questions__title">
-                  Возможно ли продлить срок участия в программе, если и я и мой «младший» хотим остаться в программе?
-                </h2>
-              </a>
-              <p className="rubric question__rubric main-questions__rubric">рубрика</p>
-            </article>
-
-            <article className="question main-questions__item">
-              <a href="./questions.html" className="main-questions__link">
-                <h2 className="section-title question__title main-questions__title">
-                  Что делать если Ваш младший решил закрыть пару, т.к. слишком занят с учебой и друзьями?
-                </h2>
-              </a>
-              <p className="rubric question__rubric main-questions__rubric">рубрика</p>
-            </article>
+          <div className='main-questions'>
+            {questions.map((question) => (
+              <Question key={question.id} title={question.title} tags={question.tags} />
+            ))}
           </div>
         </article>
       </section>
 
-      <section className="main-section page__section">
-        <article className="card card_color_green card_content_quote">
-          <a href="./articles.html" className="card__link-wrap">
-            <h3 className="chapter-title card__quote">
+      <section className='main-section page__section'>
+        <article className='card card_color_green card_content_quote'>
+          <a href='./articles.html' className='card__link-wrap'>
+            <h3 className='chapter-title card__quote'>
               У таких детей возникает ощущение отверженности. Оно приводит к напряженности и недоверию к людям и, как
               итог, к реальному неприятию себя и окружающих.
             </h3>
           </a>
-          <a href="./articles.html" className="link card__link">
+          <a href='./articles.html' className='link card__link'>
             читать статью
           </a>
         </article>
@@ -136,6 +137,31 @@ function Main({ loggedIn }) {
 
 Main.propTypes = {
   loggedIn: PropTypes.bool.isRequired,
+  history: PropTypes.objectOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      imageUrl: PropTypes.string,
+      title: PropTypes.string,
+    })
+  ).isRequired,
+  place: PropTypes.objectOf(
+    PropTypes.shape({
+      chosen: PropTypes.bool,
+    })
+  ).isRequired,
+  video: PropTypes.objectOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      duration: PropTypes.number,
+      imageUrl: PropTypes.string,
+      info: PropTypes.string,
+      link: PropTypes.string,
+      title: PropTypes.string,
+    })
+  ).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  questions: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  articles: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 export default Main;
