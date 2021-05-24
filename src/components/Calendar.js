d5d8ed79d0f04dfbd087f6c1f4e047ffb3a89b6b
Sseverniy <goodblessyo@yandex.ru>
2021-05-25 02:30:20 +0500

    feat: создать генерацию тегов месяца по событиям

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,26 @@
 import React from "react";
-import {Helmet} from 'react-helmet';
-import PropTypes from 'prop-types';
+import {Helmet} from "react-helmet";
+import PropTypes from "prop-types";
+import { format } from "date-fns";
+import ruLocale from "date-fns/locale/ru";
 import Meetup from "./Meetup";
 import Title from "./Title";
-// eslint-disable-next-line react/prop-types
+
 function Calendar({toggleModal, events}) {
+
+  function getUniqueMonths() {
+    // функция, которая перебирает события и возвращает массив с названиями месяцев
+    const monthArray = events.map((event) => {
+      const newMonth = (format(new Date(event.startAt), 'LLLL', { locale: ruLocale })).toString();
+      return newMonth;
+    });
+    console.log(monthArray);
+    // возвращаем только уникальные значения месяцев
+    return Array.from(new Set(monthArray));
+  }
+  const uniqueMonths = getUniqueMonths();
+  console.log(uniqueMonths)
+
   return (
     <>
       <Helmet>
@@ -12,7 +28,16 @@ function Calendar({toggleModal, events}) {
         <meta name="description" content="Календарь событий и мероприятий для настаников"/>
         <meta name="keywords" content="Календарь, мероприятия, события"/>
       </Helmet>
-      <Title />
+      <section className="lead page__section">
+        <h1 className="main-title">Календарь</h1>
+        <div className="tags">
+          <ul className="tags__list">
+            {uniqueMonths.map((month) =>
+              <Title month={month}/>
+            )}
+          </ul>
+        </div>
+      </section>
       <section className="calendar-container page__section">
         {events.map((event) => <Meetup toggleModal={toggleModal} event={event}/>)}
       </section>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,80 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// import { format } from 'date-fns';
+// import ruLocale from 'date-fns/locale/ru';
 
-function Title() {
+function Title({ month }) {
+  const [isChose, setIsChose] = React.useState(false);
+  const monthTagClassName = (
+    `button tags__button ${isChose ? 'tags__button_active' : ''}`
+  );
+
+  function chooseMonthTag(e) {
+    setIsChose(!isChose);
+    console.log(e.target.innerText);
+  }
+
+  const finalMonthName = month[0].toUpperCase() + month.substring(1);
+
   return (
-    <section className="lead page__section">
-      <h1 className="main-title">Календарь</h1>
-      <div className="tags">
-        <ul className="tags__list">
-          <li className="tags__list-item">
-            <button
-              className="button tags__button tags__button_active"
-              type="button"
-            >
-              Декабрь
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Январь
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Февраль
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Март
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Апрель
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Май
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Июнь
-            </button>
-          </li>
-        </ul>
-        <ul className="tags__list">
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Июль
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Август
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Сентябрь
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Октябрь
-            </button>
-          </li>
-          <li className="tags__list-item">
-            <button className="button tags__button" type="button">
-              Ноябрь
-            </button>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <li className='tags__list-item'>
+      <button className={monthTagClassName} type='button' onClick={chooseMonthTag}>
+        {finalMonthName}
+      </button>
+    </li>
   );
 }
+
+Title.propTypes = {
+  month: PropTypes.string
+  .isRequired
+};
 
 export default Title;
