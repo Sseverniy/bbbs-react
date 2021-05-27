b8f30a075ff32cc930a2fa9ba691305f400a29c3
Victor Zhabolenko <kenzbrug1337@yandex.ru>
2021-05-27 20:03:09 +0300

    fix: profile+header, +маленькие исправления

--- src/components/PersonalArea.js
+++ src/components/PersonalArea.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import MeetingReviewCard from './MeetingReviewCard';
 
-function PersonalArea({ isLogOut }) {
+function PersonalArea() {
   return (
     <section className='personal-area page__section'>
       <Helmet>
@@ -12,15 +12,22 @@ function PersonalArea({ isLogOut }) {
         <meta name='description' content='Личный кабинет наставника' />
         <meta name='keywords' content='Личный кабинет' />
       </Helmet>
-      <div className='personal-area__user-info'>
-        <Link
-          to='/'
-          className='paragraph personal-area__user-link personal-area__user-link_type_exit'
+      {/* <div className='personal-area__user-info'>
+        <p className='paragraph personal-area__user-link personal-area__user-link_type_city'>{`Москва. `}</p>
+        <button
+          type='button'
+          className='paragraph personal-area__user-link personal-area__user-link_type_exit personal-area__button-top'
+        >
+          Изменить город
+        </button>
+        <button
+          type='button'
+          className='paragraph personal-area__user-link personal-area__user-link_type_exit personal-area__button-top'
           onClick={isLogOut}
         >
           Выйти
-        </Link>
-      </div>
+        </button>
+      </div> */}
       <div className='personal-area__sign-up'>
         <h2 className='section-title personal-area__title personal-area__title_type_sign-up'>
           У вас нет записи на мероприятия
@@ -38,8 +45,8 @@ function PersonalArea({ isLogOut }) {
   );
 }
 
-PersonalArea.propTypes = {
-  isLogOut: PropTypes.func.isRequired,
-};
+// PersonalArea.propTypes = {
+//   isLogOut: PropTypes.func.isRequired,
+// };
 
 export default PersonalArea;
